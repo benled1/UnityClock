d85063ae626021c125860d378411565f4e262f46
Ben Ledingham <benledingham1@gmail.com>
2024-01-31 23:37:57 -0800

    do a bit of studying on quaternions and euler angles

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Scripting;
 
 public class Clock : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    Transform hoursPivot, minutesPivot, secondsPivot;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Awake() {}
 }
