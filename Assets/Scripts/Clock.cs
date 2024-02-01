22ad50611de4fd2eea8bf031af40ea6803437884
Ben Ledingham <benledingham1@gmail.com>
2024-02-01 09:30:25 -0800

    clock done :)

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -1,15 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
-using UnityEngine.Scripting;
 
 public class Clock : MonoBehaviour
 {
     [SerializeField]
     Transform hoursPivot, minutesPivot, secondsPivot;
+    const float oneStepHourRotation = -30f;
+    const float oneStepMinuteRotation = -6f;
+    const float oneStepSecondRotation = -6f;
 
-    void Awake() {
-        hoursPivot.localRotation = Quaternion.Euler(0,0,-30);
-
+    void Update() {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        Debug.Log(oneStepSecondRotation * (float)time.TotalSeconds);
+        hoursPivot.localRotation = Quaternion.Euler(0,0,oneStepHourRotation * (float)time.TotalHours);
+        minutesPivot.localRotation = Quaternion.Euler(0,0,oneStepMinuteRotation * (float)time.TotalMinutes);
+        secondsPivot.localRotation = Quaternion.Euler(0,0,oneStepSecondRotation * (float)time.TotalSeconds);
     }
 }
